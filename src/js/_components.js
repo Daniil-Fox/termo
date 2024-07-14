361bbd67d3d19a94e01ee120687ecfdef91ff829
Artyushenko Daniil <foxones80@gmail.com>
2024-07-15 01:18:51 +0600

    added response

--- src/js/_components.js
+++ src/js/_components.js
@@ -1,2 +1,3 @@
 import "./components/select.js";
 import "./components/tabs.js";
+import "./components/sliders.js";
---
+++ src/js/components/sliders.js
@@ -0,0 +1,52 @@
+import { Swiper } from "swiper";
+
+new Swiper(".types__slider", {
+  slidesPerView: "auto",
+  spaceBetween: 20,
+});
+
+window.addEventListener("DOMContentLoaded", () => {
+  const resizableSwiper = (
+    breakpoint,
+    swiperClass,
+    swiperSettings,
+    callback
+  ) => {
+    let swiper;
+
+    breakpoint = window.matchMedia(breakpoint);
+
+    const enableSwiper = function (className, settings) {
+      swiper = new Swiper(className, settings);
+
+      if (callback) {
+        callback(swiper);
+      }
+    };
+
+    const checker = function () {
+      if (breakpoint.matches) {
+        return enableSwiper(swiperClass, swiperSettings);
+      } else {
+        if (swiper !== undefined) swiper.destroy(true, true);
+        return;
+      }
+    };
+
+    breakpoint.addEventListener("change", checker);
+    checker();
+  };
+
+  const someFunc = (instance) => {
+    if (instance) {
+      instance.on("slideChange", function (e) {
+        console.log("*** mySwiper.activeIndex", instance.activeIndex);
+      });
+    }
+  };
+
+  resizableSwiper("(max-width: 768px)", ".cond__slider", {
+    slidesPerView: "auto",
+    spaceBetween: 15,
+  });
+});
